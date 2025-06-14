cb61c277c22bd94013ba10ac55466754f8d14358
jshprz <joshuaperezmelgazo@gmail.com>
2025-06-14 10:53:14 +0800

    Reusable data table component

--- src/app/features/asset-discovery/asset-discovery.component.ts
+++ src/app/features/asset-discovery/asset-discovery.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { TableToolbarComponent } from '../../shared/components/card/table-toolbar/table-toolbar.component';
+import { TableToolbarComponent } from '../../shared/components/table/table-toolbar/table-toolbar.component';
+import { DataTableComponent } from '../../shared/components/table/data-table/data-table.component';
 
 @Component({
   selector: 'app-asset-discovery',
-  imports: [TableToolbarComponent],
+  imports: [TableToolbarComponent, DataTableComponent],
   templateUrl: './asset-discovery.component.html',
   styleUrl: './asset-discovery.component.css',
 })
